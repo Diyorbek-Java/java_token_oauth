80e013072a99f6faa40295df500f5c27dd93b437
Diyorbek-Java <diorbeckturdiev@gmail.com>
2025-09-15 10:08:44 +0500

    outh with github done

---
+++ src/main/java/uz/wiut/java_token_testing/controller/HomeController.java
@@ -0,0 +1,16 @@
+package uz.wiut.java_token_testing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+    @GetMapping("/secured")
+    public String secured() {
+        return "Secured";
+    }
+}
