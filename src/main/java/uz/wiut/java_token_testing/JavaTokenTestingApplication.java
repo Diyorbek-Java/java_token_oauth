c737ac28dbfe9c95603b3796a5592aa6868011d8
Diyorbek-Java <diorbeckturdiev@gmail.com>
2025-09-15 09:30:22 +0500

    first commit

---
+++ src/main/java/uz/wiut/java_token_testing/JavaTokenTestingApplication.java
@@ -0,0 +1,13 @@
+package uz.wiut.java_token_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTokenTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTokenTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/wiut/java_token_testing/JavaTokenTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.wiut.java_token_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTokenTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
